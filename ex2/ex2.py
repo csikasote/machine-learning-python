5d201e92c45538f1a3e4592e181cc38c07ca8885
Claytone Sikasote <claytone.sikasote@outlook.com>
2019-08-15 10:11:30 +0200

    # Added the function to visualize training data

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -2,12 +2,16 @@
 from scipy import optimize as opt
 import pandas as pd
 import numpy as np
-import sys
 import os
 
 # Matplotlib for plotting figures
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+
+# Where to save the figures
+EXERCISE_ROOT_DIR = "."
+IMAGES_PATH = os.path.join(EXERCISE_ROOT_DIR, "images")
 
 # Defining a class Logistic Regression that implements ex2
 class LogisticRegression(object):
@@ -17,10 +21,15 @@ class LogisticRegression(object):
     # Sigmoid function
     def sigmoid(self, z):
         return (1/(1+ np.exp(-z)))
+
+    # Net Input
+    def net_input(self,X,theta):
+        return np.dot(X,theta)
     
     # Objective function
     def cost_function(self,theta,X,y):
-        y_hat = self.sigmoid(np.dot(X,theta))
+        z = self.net_input(X,theta)
+        y_hat = self.sigmoid(z)
         return (-1/len(X)) * (np.dot(y.T,np.log(y_hat)) +\
                               np.dot((1-y).T,np.log(1-y_hat)))
 
@@ -33,27 +42,62 @@ class LogisticRegression(object):
                         full_output=True)
     # Predict function
     def predict(self,X,theta):
-        probability = self.sigmoid(np.dot(X,theta))
+        z = self.net_input(X,theta)
+        probability = self.sigmoid(z)
         return [1 if p >= 0.5 else 0 for p in probability]
 
     # Classification function
-    def classify(self,X,theta):  
-        probability = self.sigmoid(np.dot(X,theta))
+    def classify(self,X,theta):
+        z = self.net_input(X,theta)
+        probability = self.sigmoid(z)
         return ["ADMIT" if p >= 0.5 else "DECLINE" for p in probability]
 
     # Decision function
     def decision_(self,X,theta):
-        return "\nProbability: %2f"%(self.sigmoid(np.dot(X,theta))[0])\
+        z = self.net_input(X,theta)
+        return "\nProbability: %2f"%(self.sigmoid(z)[0])\
                + ' \nPrediciton: ' + str(self.predict(X,theta)[0]) \
                + ' \nModel Decision: ' + str(self.classify(X,theta)[0]) \
 
-    
+# Function to plot the data distribution
+def plot_data(x1,x2,y1,y2): 
+    plt.scatter(x1,y1, s=50, c='b', marker='o', label='Admitted')  
+    plt.scatter(x2,y2, s=50, c='r', marker='x', label='Not Admitted')  
+    plt.xlabel('Exam 1 Score')  
+    plt.ylabel('Exam 2 Score')
+    plt.title('Scatter plot of Trainig data')
+    plt.axis([20,110,20,110])
+    plt.grid(True)
+    plt.legend()
+
+# The function allows images to be saved
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
 def main():
     # Load dataset
     path = os.getcwd() + '\data\ex2data1.txt'
-    data=pd.read_csv(path, header=None, names=['Exam 1', 'Exam 2', 'Admitted']).values
-    X = data[:,[0,1]] 
-    y = data[:,[2]]
+    df = pd.read_csv(path, header=None, names=['Exam 1', 'Exam 2', 'Admitted'])
+    pos_df = df[df['Admitted'].isin([1])]
+    neg_df = df[df['Admitted'].isin([0])]
+    Ex1_pos = pos_df['Exam 1'] 
+    Ex2_pos = pos_df['Exam 2']
+    Ex1_neg = neg_df['Exam 1']
+    Ex2_neg = neg_df['Exam 2']
+
+    # Visualize the data distribution
+    input("Press <ENTER> to visualize data ...")
+    plot_data(Ex1_pos,Ex1_neg,Ex2_pos,Ex2_neg)
+    save_fig("data_plot")
+    plt.show()
+
+    # Extracting Features and target labels
+    X = df.values[:,[0,1]] 
+    y = df.values[:,[2]]
     
     # Add a column of ones to x for x0 = 1
     X = np.c_[np.ones((len(X), 1)), X]
