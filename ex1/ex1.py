67cad8401ac52c096ed42504f34c97aa4dbd21ee
Claytone Sikasote <claytone.sikasote@outlook.com>
2019-08-25 21:08:36 +0200

    # Modified the file

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,62 +1,142 @@
 # Import common python libraries
 import numpy as np
+import pandas as pd
 import os
 
 # Matplotlib for plotting figures
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-# Import helper function
-from linear_model_utils import load_data, plot_data, save_fig
-from linear_model_utils import plot_cost, bgd_path, plot_fit
-from linear_model_utils import SimpleLinearRegressionGD
+# Where to save the figures
+EXERCISE_ROOT_DIR = "."
+IMAGES_PATH = os.path.join(EXERCISE_ROOT_DIR, "images")
 
-# Load data for computation
-print('Loading data ... ')
-file = os.getcwd() + '\data\ex1data1.txt'
-X,y = load_data(file)
+# Simple linear regression class
+class SimpleLinearRegressionGD(object):
+    def __init__(self, alpha=0.001, n_iter=20):
+        self.alpha    = alpha
+        self.n_iter   = n_iter
+    
+    def MSE(self, X, y):
+        self.w_  = np.zeros(X.shape[1])
+        y_hat    = self.hypothesis(X)
+        errors   = (y_hat - y)
+        squared_errors = errors**2
+        return (1/(2*len(y))) * squared_errors.sum()
 
-# Adding the bias term to the dataset
-input("\nPress <ENTER> key to visualize dataset ..")
-Xtrain = np.insert(X,0,1,axis=1)
-ytrain = y
+    def fit(self, X, y):
+        self.w_    = np.zeros(X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            y_hat    = self.hypothesis(X)
+            errors   = (y_hat - y)
+            gradient = self.alpha * np.dot(X.T, errors)
+            self.w_  -= self.alpha * gradient
+            cost     = np.sum(errors**2) / (len(y)*2.0)
+            self.cost_.append(cost)
+        return self
+        
+    def hypothesis(self, X):
+        return np.dot(X,self.w_)
 
-# Plotting the data set
-plot_data(X,y)
-#save_fig("training_data")
-plt.show(block=False)
+    def predict(self, X):
+        return self.hypothesis(X)
 
-# Creating an object of linear regression
-lr = SimpleLinearRegressionGD(0.01, 1500)
 
-# Computing initial cost
-input("\nPress <ENTER> to compute initial cost ...")
-print('\nInitial cost computes is %.2f.'%(lr.MSE(Xtrain,ytrain)))
+# The function allows images to be saved
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
 
-# Fit the SLR
-print('\nRunning linear regression with BGD ... ', end='')
-input("")
-lr.fit(Xtrain, ytrain)
-print("\nMinimum point(using BGD):", lr.w_)
-print('\nLinear Model: Y = %.3f + %.3fx1'%(lr.w_[0], lr.w_[1]))
+# plotData() function to visualize the distribution of training data
+def plot_data(X,y):
+    plt.plot(X,y,'rx',markersize=10, label='Training Example')
+    plt.grid(True)
+    plt.ylabel('Profit in $10,000s')
+    plt.xlabel('Population of City in 10,000s')
+    plt.title('Training data')
+    plt.axis([5, 25, -5, 25])
+    plt.legend()
 
-# Plot Cost vs. Iterations graph
-input("\nPress <ENTER> key to cost vs. iteration graph ...")
-plot_cost(lr)
-#save_fig("BGD")
-plt.show(block=False)
+# Function to plot the cost change against # of iterations for SLR 
+def plot_cost(model):
+    plt.plot(range(1, model.n_iter+1), model.cost_, 'b-o', label= r'${J{(\theta)}}$')
+    plt.xlabel('# of Iterations')
+    plt.ylabel(r'${J{(\theta)}}$', rotation=1)
+    plt.xlim([0,model.n_iter])
+    plt.ylim([4,7])
+    plt.grid(True)
+    plt.title('Batch Gradient Descent (BGD)')
+    plt.legend()
 
-# Plot of the fitting line on dataset
-input("\nPress <ENTER> key to plot a fit line ...")
-plot_fit(Xtrain,X,y,lr)
-#save_fig("fit_line")
-plt.show(block=False)
+# Fit SLR
+def plot_fit(Xtrain,X,y,model):
+    plt.plot(X,y,'rx',markersize=10, label='Training Example')
+    plt.plot(Xtrain, model.predict(Xtrain),'--', color='blue', lw=2)
+    plt.grid(True) 
+    plt.ylabel('Profit in $10,000s')
+    plt.xlabel('Population of City in 10,000s')
+    titler = 'Linear Model: Y = %.3f + %.3fx1'%(model.w_[0], model.w_[1])
+    plt.title(titler)
+    plt.axis([5, 25, -5, 25])
+    plt.legend()
 
-# Making prediction
-print('\nPredictions ... ', end='')
-input("Press <ENTER> key to make prediction ...")
-print('\nPopulation\t\t Profit\n==========\t\t ======')
-print('%.f \t\t\t %.2f'%(35000, lr.predict([1, 3.5]) * 10000))
-print('%.f \t\t\t %.2f\n'%(70000, lr.predict([1, 7.0]) * 10000))
+def main():
+    print("\nProgramming Exercise 1: Linear Regression with Single Variable\n")
+    # Load dataset for computation
+    data = pd.read_csv(os.getcwd() + '\data\ex1data1.txt', header=None).values
+    X = data[:,0].reshape(-1,1)
+    y = data[:,1]
 
-input("Press <ENTER> key to continue ...")
+    # Adding the bias term to the dataset
+    input("\nPress <ENTER> key to visualize dataset ...")
+    Xtrain = np.insert(X,0,1,axis=1)
+    ytrain = y
+
+    # Visualize the dataset
+    plt.figure(1)
+    plot_data(X,y)
+    plt.show(block=False)
+
+    # Instantiating an object of linear regression class
+    lr = SimpleLinearRegressionGD(0.01, 1500)
+
+    # Computing initial cost
+    input("\nPress <ENTER> to compute initial cost ...")
+    print('\nInitial cost computes is %.2f.'%(lr.MSE(Xtrain,ytrain)))
+
+    # Fit the SLR
+    input("\nPress <ENTER> key to run SLR with BGD ...")
+    lr.fit(Xtrain, ytrain)
+    print("\n\nMinimum point(using BGD):", lr.w_)
+    print('\nLinear Model: Y = %.3f + %.3fx1'%(lr.w_[0], lr.w_[1]))
+
+    # Plot Cost vs. Iterations graph
+    input("\nPress <ENTER> key to cost vs. iteration graph ...")
+    plt.figure(2)
+    plot_cost(lr)
+    plt.show(block=False)
+
+    # Plot of the fitting line on dataset
+    input("\nPress <ENTER> key to plot a fit line ...")
+    plt.figure(3)
+    plot_fit(Xtrain,X,y,lr)
+    plt.show(block=False)
+
+    # Predictions based on the computed weights
+    print('\nPredictions ... ', end='')
+    input("Press <ENTER> key to make prediction ...")
+    print('\nPopulation\t\t Profit\n==========\t\t ======')
+    print('%.f \t\t\t %.2f'%\
+          (35000, lr.predict([1, 3.5]) * 10000))
+    print('%.f \t\t\t %.2f\n'%\
+          (70000, lr.predict([1, 7.0]) * 10000))
+
+    # Terminate the program
+    input("Press <ENTER> key to terminate program ...")
+
+if __name__ == "__main__":
+    main()
--- ex1/ex1_multi.py
+++ ex1/ex1_multi.py
@@ -8,51 +8,119 @@ import os
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-# IMPORT HELPER FUNCTIONS
-from linear_model_utils import featureNormalize
-from linear_model_utils import plot_mlr_bgd, save_fig
-from linear_model_utils import MultivariateLinearRegressionGD
-from linear_model_utils import normalEquation
+# Where to save the figures
+EXERCISE_ROOT_DIR = "."
+IMAGES_PATH = os.path.join(EXERCISE_ROOT_DIR, "images")
 
-# STEP 1: IMPORTING DATA FILES
-path = os.getcwd() + '\data\ex1data2.txt' #PATH TO THE DATASET
-data = pd.read_csv(path, header=None).values
-X = data[:,:2]
-y = data[:,2]
+# The function allows images to be saved
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
 
-# NORMALIZE FEATURES
-X_norm, mu, sigma = featureNormalize(X)
-Xtrain = np.insert(X_norm,0,1,axis=1)
+# Multivariate linear regression class
+class MultivariateLinearRegressionGD(object):
+    def __init__(self, alpha=0.001, n_iter=20, print_cost=False):
+        self.alpha = alpha
+        self.n_iter = n_iter
+        self.print_cost = print_cost
 
+    def cost_function(self, X, y):
+        y_hat = self.hypothesis(X)
+        return (1/(2*len(y))) * (np.dot((y_hat-y).T,(y_hat-y)))
 
-# CREATE AN OBJECT OF 'MultivariateLinearRegression'
-input("\nPress <ENTER> key to run regression using BGD ...")
-print(" ")
-mlr = MultivariateLinearRegressionGD(alpha=0.1, n_iter=50, print_cost=True)
-mlr.fit(Xtrain,y)
-#print("\nThe minimum point(found by BGD) is %s"%(str(mlr.w_)))
+    def fit(self, X, y):
+        self.w_    = np.zeros(X.shape[1])
+        self.cost_ = []
+        print("Iterations\t\tCost\n==========\t\t====")
+        for i in range(self.n_iter):
+            y_hat    = self.hypothesis(X)
+            errors   = (y_hat - y)
+            self.w_ -= self.alpha * (1/len(y)) * np.dot(X.T,errors)
+            cost     = (1/(2*len(y))) * (np.dot((y_hat-y).T,(y_hat-y)))
+            self.cost_.append(cost)
+            if self.print_cost and i % 10 == 0:
+                print("{}\t\t\t{}".format(i,cost),\
+                      file=sys.stdout,\
+                      flush=True)
+        return self
 
-# PLOT THE BGD COST GRAPH
-#input("\nPress <ENTER> key to plot the BGD cost graph ...")
-#plot_mlr_bgd(mlr)
-#save_fig("MLR_COST_GRAPH")
-#plt.show()
+    def hypothesis(self,X):
+        return np.dot(X,self.w_)
 
-# TESTING THE MODEL
-test_x = [[1650, 3]]
-norm_x = np.divide((test_x - mu),sigma)
-test_x = np.insert(norm_x,0,1,axis=1)
-price  = mlr.predict(test_x)
-print('\nPrice of a 1650 sq-ft, 3 br house (using BGD): $%.2f'%(price))
+    def predict(self,X):
+        return self.hypothesis(X)
 
-input("\nPress <ENTER> key to run regression using Normal Equation ...")
-# Computing theta using Normal Equation
-theta = normalEquation(Xtrain, y)
-#print("\nThe minimum point(found by NormalEquation) is %s"%(str(theta)))
-price = np.dot(test_x,theta)
-print('\nPrice of a 1650 sq-ft, 3 br house (using NEp): $%.2f' %(price))
-input("\nPress <ENTER> key to terminate the process ...")
+# Normal equation for linear equations
+def normalEquation(X, y):
+    return np.dot(np.dot(np.linalg.pinv(np.dot(X.T,X)), X.T),y)
 
+# Function to normalize features
+def featureNormalize(X):
+    # Computing the Mean and STD of X
+    mu = np.mean(X, axis=0, keepdims=True);
+    sigma = np.std(X, axis = 0, keepdims=True);
+    X_norm = (X - mu)/sigma
+    return X_norm, mu, sigma
 
+# Function to plot the cost change against # of iterations for MLR 
+def plot_mlr_bgd(model):
+    g = plt.figure(1)
+    plt.plot(range(1, model.n_iter+1), model.cost_, 'g-s', label= r'${J{(\theta)}}$')
+    plt.grid(True)
+    learning_rate = r"$\alpha = {}$".format(model.alpha)
+    plt.title("BGD with " + str(learning_rate) + " learning rate") 
+    plt.xlabel('# of Iterations')
+    plt.ylabel(r'${J{(\theta)}}$', rotation=1)
+    plt.axis([0,50,0,7e10])
+    plt.legend()
 
+def main():
+    print("\nProgramming Exercise 1: Multivariate Linear Regression")
+    # Load dataset for computation
+    input("\nPress <ENTER> key to load dataset ...\n")
+    data = pd.read_csv(os.getcwd() + '\data\ex1data2.txt', header=None).values
+    X = data[:,:2]
+    y = data[:,2]
+    print("... dataset loaded successfully!")
+
+    # Normalize features
+    X_norm, mu, sigma = featureNormalize(X)
+    Xtrain = np.insert(X_norm,0,1,axis=1)
+
+    # CREATE AN OBJECT OF 'MultivariateLinearRegression'
+    input("\nPress <ENTER> key to run MLR(using BGD) ...")
+    mlr = MultivariateLinearRegressionGD(alpha=0.1, n_iter=50, print_cost=True)
+    mlr.fit(Xtrain,y)
+    print("\nThe minimum point(found by BGD) is \n%s"%(str(mlr.w_)))
+
+    # PLOT THE COST GRAPH
+    input("\nPress <ENTER> key to plot the BGD graph ...")
+    plt.figure(1)
+    plot_mlr_bgd(mlr)
+    plt.show(block=False)
+
+    # Testing model
+    input("\nPress <ENTER> key to test model ...\n")
+    test_x = [[1650, 3]]
+    print("Test example:",test_x)
+    norm_x = np.divide((test_x - mu),sigma)
+    test_x = np.insert(norm_x,0,1,axis=1)
+    price  = mlr.predict(test_x)
+    print('\nPrice of a 1650 sq-ft, 3 br house (using BGD): $%.2f'%(price))
+    input("\nPress <ENTER> key to run regression using Normal Equation ...")
+    
+    # Computing theta using Normal Equation
+    theta = normalEquation(Xtrain, y)
+    print("\nThe minimum point(found by NormalEquation) is \n%s"%(str(theta)))
+    price = np.dot(test_x,theta)
+    print('\nPrice of a 1650 sq-ft, 3 br house (using NEp): $%.2f' %(price))
+
+    # Terminate program
+    input("\nPress <ENTER> key to terminate the process ...")
+
+if __name__ == "__main__":
+    main()
 
